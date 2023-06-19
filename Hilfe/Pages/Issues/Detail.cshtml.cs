0ecbdc260b410257165841fe7a97015559fcc89e
Ahurein Ebenezer <ahureinebenezer@gmail.com>
2023-06-19 09:52:20 +0000

    resolve issue

--- Hilfe/Pages/Issues/Detail.cshtml.cs
+++ Hilfe/Pages/Issues/Detail.cshtml.cs
@@ -14,6 +14,17 @@ namespace Hilfe.Pages.Issues
             Issue = await _context.Issues.FindAsync(id);
         }
 
+        public async void OnGetResolve(uint id)
+        {
+            var issueToUpdate = _context.Issues.SingleOrDefault(i => i.Id == id);
+            if (issueToUpdate == null) return;
+
+            issueToUpdate.Completed = DateTime.Now;
+            _context.Update(issueToUpdate);
+            await _context.SaveChangesAsync();
+
+        }
+
         public Issue? Issue { get; private set;}
     }
 }
